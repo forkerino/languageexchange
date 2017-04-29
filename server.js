2a7e8214e12c447431db826135e1d74a59b0bf3c
forkerino <pieterstokkink@gmail.com>
2017-04-29 15:50:54 +0200

    added User Schema

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import passport from 'passport';
 
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 const app = express();
+
+mongoose.connect(process.env.MLAB_URI);
 
 app.get('/', (req, res)=> res.end("Hello world"));
 
